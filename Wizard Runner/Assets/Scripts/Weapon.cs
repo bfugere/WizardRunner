5ab4b3fba658b740836431a24a03a1c1d49312bd
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-12 12:20:14 -0500

    Weapon VFX

--- Wizard Runner/Assets/Scripts/Weapon.cs
+++ Wizard Runner/Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 25f;
+    [SerializeField] ParticleSystem staffVFX;
     
     void Update()
     {
@@ -19,13 +20,24 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        PlayStaffVFX();
+        ProcessRaycast();
+    }
+
+    void PlayStaffVFX()
+    {
+        staffVFX.Play();
+    }
+
+    void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("Hit: " + hit.transform.name);
             // TODO: Add hit effects.
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if (target == null) 
+            if (target == null)
                 return;
             target.TakeDamage(damage);
         }
