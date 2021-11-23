8966406f193e54fecd3813cb84b45698431477ed
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-23 12:46:12 -0500

    Level & Lighting Design

---
+++ Wizard Runner/Assets/Scripts/FlashlightSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .2f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle)
+            return;
+        else
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+    }
+
+    void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
